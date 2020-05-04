498224499e927492e901f141ae23013138d4922b
Rohan Padshah <rohanpadshah68@gmail.com>
2020-05-04 23:49:22 +0530

    Designed 1st test case.

--- LeapYearUsingTDD/src/main/java/com/technicalassignment/LeapYearUsingTDD/Year.java
+++ LeapYearUsingTDD/src/main/java/com/technicalassignment/LeapYearUsingTDD/Year.java
@@ -1,5 +1,14 @@
 package com.technicalassignment.LeapYearUsingTDD;
 
 public class Year {
+	
+	int year;
+	
+	public Year(int year) {
+		this.year = year;
+	}
 
+	public boolean isLeapYear() {
+		return false;
+	}
 }
--- LeapYearUsingTDD/src/test/java/com/technicalassignment/LeapYearUsingTDD/LeapYearTest.java
+++ LeapYearUsingTDD/src/test/java/com/technicalassignment/LeapYearUsingTDD/LeapYearTest.java
@@ -7,8 +7,10 @@ import org.junit.Test;
 public class LeapYearTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void isLeapYearIfYearIsDivisibleBy400()
+	{
+		Year year = new Year(2000);
+		boolean actual = year.isLeapYear();
+		assertEquals(true, actual);
 	}
-
 }
