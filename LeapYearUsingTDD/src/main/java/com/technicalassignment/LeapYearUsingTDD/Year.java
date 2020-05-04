4fe02e85a6609726c64aa0fb608131a6907ce5dc
Rohan Padshah <rohanpadshah68@gmail.com>
2020-05-05 01:13:06 +0530

    Refator the leap year code to make it cleaner

--- LeapYearUsingTDD/src/main/java/com/technicalassignment/LeapYearUsingTDD/Year.java
+++ LeapYearUsingTDD/src/main/java/com/technicalassignment/LeapYearUsingTDD/Year.java
@@ -10,19 +10,28 @@ public class Year {
 
 	public boolean isLeapYear() {
 		
-		if(year < 0)
-			return false;
-		
-		if(year % 400 == 0)
+		if(isValidYear() && isYearMultipleOf400() || (isYearMultipleOf4() && isYearNotMultipleOf100()))
 			return true;
-		
-		if(year % 100 == 0)
-			return false;
-		
-		if(year % 4 == 0)
-			return true;
-		
 		return false;
-		
 	}
+	public boolean isYearMultipleOf400()
+	{
+		return year % 400 == 0;
+	}
+	
+	public boolean isYearNotMultipleOf100()
+	{
+		return !(year % 100 == 0);
+	}
+	
+	public boolean isYearMultipleOf4()
+	{
+		return year % 4 == 0;
+	}
+	
+	public boolean isValidYear()
+	{
+		return !(year < 0);
+	}
+	
 }
